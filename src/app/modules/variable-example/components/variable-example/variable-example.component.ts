7d2367731a0b6f7c5050fd30f95dfad88b6dfa07
FilMat91 <91672309+FilMat91@users.noreply.github.com>
2022-05-25 16:11:45 +0200

    create variables components

--- src/app/modules/variable-example/components/variable-example/variable-example.component.ts
+++ src/app/modules/variable-example/components/variable-example/variable-example.component.ts
@@ -9,7 +9,52 @@ export class VariableExampleComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  get getVarString() : string
+  {
+    var output = "First output";
+    var number = 5;
+    if(number > 4)
+    {
+      var output = "Second output";
+    }
+
+    return output;
   }
 
+  get getLetString() : string
+  {
+    let output = "First output";
+    let number = 5;
+    if(number > 4)
+    {
+      let output = "Second output";
+    }
+
+    return output;
+  }
+
+  get getConstString() : string
+  {
+    const output = "First output";
+    const number = 5;
+    if(number > 4)
+    {
+      const output = "Second output";
+    }
+
+    //output = 5; //Le constanti non sono modificabili a meno che non siano oggetti e la motifica è su uno specifico attributo
+    return output;
+  }
+
+  getText(input: string) {
+    return [`${input} output = "First output";`,
+            `${input} number = 5;`,
+            `if(number > 4)`,
+            `{`,
+            `${input} output = "Second output";`,
+            `}`,
+            `return output;`];
+  }
 }
